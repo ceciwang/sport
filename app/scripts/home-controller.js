bbff58bcf07d7f2605b99f4d68cbc60c10db1131
wang chao <cwang@thoughtworks.com>
2014-04-05 19:21:48 +0800

    add images

--- app/scripts/home-controller.js
+++ app/scripts/home-controller.js
@@ -4,15 +4,19 @@ function HomeController($scope) {
     $scope.myInterval = 5000;
 
     var slides = $scope.slides = [];
+    var imageUris = _.map(["huawei", "nike", "nike2", "nike3"], function(url){
+        return "images/"+url+".jpg";
+    });
+
     $scope.addSlide = function() {
         var newWidth = 600 + slides.length;
         slides.push({
-            image: 'http://placekitten.com/' + newWidth + '/300',
             text: ['More','Extra','Lots of','Surplus'][slides.length % 4] + ' ' +
                 ['Cats', 'Kittys', 'Felines', 'Cutes'][slides.length % 4]
         });
     };
     for (var i=0; i<4; i++) {
         $scope.addSlide();
+        slides[i].image = imageUris[i];
     }
 }
