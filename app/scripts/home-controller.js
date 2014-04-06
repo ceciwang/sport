674f60e26fb4e5e935eb3f82b98620b9dec87274
wang chao <cwang@thoughtworks.com>
2014-04-07 00:19:46 +0800

    change the way of using pics

--- app/scripts/home-controller.js
+++ app/scripts/home-controller.js
@@ -2,9 +2,6 @@ window.sport.controller('HomeController', ['$scope', function($scope){
     $scope.myInterval = 5000;
 
     var slides = $scope.slides = [];
-    var imageUris = _.map(["huawei","huawei","huawei","huawei"], function(url){
-        return "images/"+url+".jpg";
-    });
 
     $scope.addSlide = function() {
         var newWidth = 600 + slides.length;
@@ -15,6 +12,6 @@ window.sport.controller('HomeController', ['$scope', function($scope){
     };
     for (var i=0; i<4; i++) {
         $scope.addSlide();
-        slides[i].image = imageUris[i];
+        slides[i].image = "http://bcs.duapp.com/ccwang/sport%2Fhome%2Fhuawei.jpg";
     }
 }]);
