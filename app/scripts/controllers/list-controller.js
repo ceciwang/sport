b6b823588ab08612ce5409d0cc6f9ca3fc54081d
wang chao <cwang@thoughtworks.com>
2014-04-07 19:46:13 +0800

    add image detail view

--- app/scripts/controllers/list-controller.js
+++ app/scripts/controllers/list-controller.js
@@ -1,5 +1,10 @@
-window.sport.controller('ListController', ['$scope', 'imageRepo', function ($scope, imageRepo) {
+window.sport.controller('ListController', ['$scope', 'imageRepo','$modal', function ($scope, imageRepo, $modal) {
     imageRepo.getRiding(function(data){
         $scope.pics = data.riding;
-    })
+    });
+    $scope.open = function (imageUrl) {
+        var modalInstance = $modal.open({
+            template: '<div><img ng-src="'+imageUrl+'" alt="riding"></div>'
+        });
+    };
 }]);
