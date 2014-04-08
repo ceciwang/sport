c4cae689e8a35ee8bee18748bd89c3f5826578c9
zengfanping <zengfanping@baidu.com>
2014-04-08 23:48:42 +0800

    replace url from json content

---
+++ app/data/images.js
@@ -0,0 +1,17 @@
+{
+    "home": ["http://bcs.duapp.com/xdlink/home/home003.png",
+        "http://bcs.duapp.com/xdlink/home/home002.png",
+        "http://bcs.duapp.com/xdlink/home/home005.png",
+        "http://bcs.duapp.com/xdlink/home/home001.jpg"],
+
+    "riding": ["http://bcs.duapp.com/xdlink/second/ai-100.jpg",
+    "http://bcs.duapp.com/xdlink/second/bi-100.jpg",
+    "http://bcs.duapp.com/xdlink/second/bi-101.jpg",
+    "http://bcs.duapp.com/xdlink/second/bi-104.png"
+    ],
+    "wrestle": ["http://bcs.duapp.com/ccwang/sport%2Fwrestle%2F3.jpg",
+        "http://bcs.duapp.com/ccwang/sport%2Fwrestle%2F1.jpg",
+        "http://bcs.duapp.com/ccwang/sport%2Fwrestle%2F4.jpg",
+        "http://bcs.duapp.com/ccwang/sport%2Fwrestle%2F2.jpg"
+    ]
+}
