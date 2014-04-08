4a53c8826f774d7a40d5c4acaa5950cfdc4dfbaa
wang chao <cwang@thoughtworks.com>
2014-04-08 23:24:49 +0800

    change the route to follow breadcrumb

--- app/scripts/route.js
+++ app/scripts/route.js
@@ -1,14 +1,18 @@
 window.sport.config(['$routeProvider', function ($routeProvider) {
     $routeProvider
+        .when('/', {
+            templateUrl: 'templates/home.html',
+            label: 'Home'
+        })
         .when('/riding', {
             templateUrl: 'templates/riding.html',
             controller: 'ListController',
             label: 'Riding'
         })
-        .when('/detail/sample', {
+        .when('/riding/sample', {
             templateUrl: 'templates/riding.html',
             controller: 'DetailController',
-            label: 'Riding/Sample'
+            label: 'Sample'
         })
         .otherwise({
             templateUrl: 'templates/home.html',
