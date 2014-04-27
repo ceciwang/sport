12919e0e10b88e8820160faf7701e0cbe5c7ced7
wang chao <cwang@thoughtworks.com>
2014-04-27 23:39:54 +0800

    fix homepage

--- app/scripts/controllers/home-controller.js
+++ app/scripts/controllers/home-controller.js
@@ -1,15 +1,15 @@
-window.sport.controller('HomeController', ['$rootScope','$scope','imageRepo', function($rootScope, $scope, imageRepo){
+window.sport.controller('HomeController', ['$rootScope','$scope','imageRepo','category', function($rootScope, $scope, imageRepo, category){
     $scope.myInterval = 5000;
 
     var slides = $scope.slides = [];
 
-    var categories = ["riding", "wrestle","athletics", "sport","goods"];
+    $scope.categories = category;
 
     imageRepo.fetchHeroes().success(function(allImages){
         _.each(allImages.images, function(image, index){
             slides.push({
                 image: image,
-                uri: categories[index]
+                uri: $scope.categories[index].categoryWithLink.link
             });
         });
     });
