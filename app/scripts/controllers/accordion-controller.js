0867c92075e160233f7f793c856488187c2074ec
wang chao <cwang@thoughtworks.com>
2014-04-27 15:39:01 +0800

    update the homepage photos

--- app/scripts/controllers/accordion-controller.js
+++ app/scripts/controllers/accordion-controller.js
@@ -1,14 +1,19 @@
-window.sport.controller('AccordionController', ['$scope',"$routeParams", function ($scope, $routeParams) {
-    $scope.show = function(category){
-      return $routeParams.category === category;
+window.sport.controller('AccordionController', ['$scope', "$routeParams", function ($scope, $routeParams) {
+    $scope.show = function (category) {
+        return $routeParams.category === category;
     };
 
-    $scope.isActiveItem = function(item){
-      if($routeParams.subCategory === item){
-          return "active";
-      }
+    $scope.isActiveItem = function (item) {
+        if ($routeParams.subCategory === item) {
+            return "active";
+        }
     };
 
     $scope.groups = SPORT.categories;
 
+    $scope.getUrl = function (group, sub) {
+        var cate = group.category.join('-');
+        var item = sub.join('-');
+        return "#/" + cate + "/" + item;
+    }
 }]);
--- app/scripts/controllers/category-controller.js
+++ app/scripts/controllers/category-controller.js
@@ -1,5 +1,5 @@
 window.sport.controller('CategoryController', ['$scope', 'imageRepo','$routeParams', function ($scope, imageRepo, $routeParams) {
-    var category = $routeParams.category;
+    var category = $routeParams.category.replace('-', ' ');
 
     imageRepo.fetchImages(function(data){
         $scope.pics = data[category]["all"];
